6fc6525f98ca1b8ead36a503f2abb04ff05e5219
Z2XYU <127650018+Z2XYU@users.noreply.github.com>
2025-07-03 20:14:34 +0800

    串口监听和MQTT发送

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,9 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 //连接WIFI
-const char* ssid="zhongxu";
-const char* password="12345678";
+const char* ssid = "zhongxu";
+const char* password = "12345678";
 
 //MQTT 服务器消息
 const char* mqtt_server = "129.211.26.112";
@@ -13,15 +14,20 @@ WiFiClient esp_client;
 PubSubClient client(esp_client);
 
 //LED引脚
-const int LED_PIN=2;
+const int LED_PIN = 2;
 
-void setup_wifi(){
+//串口接收缓冲区
+String serialBuffer = "";
+
+
+
+void setup_wifi() {
   delay(10);
   Serial.println("连接WiFi...");
   WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid,password);
+  WiFi.begin(ssid, password);
 
-  while (WiFi.status()!= WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED) {
     digitalWrite(LED_PIN, !digitalRead(LED_PIN));
     delay(1000);
     Serial.print(".");
@@ -31,13 +37,13 @@ void setup_wifi(){
   Serial.println("WiFi连接成功");
 }
 
-void callback(char* topic,byte* payload,unsigned int length){
+void callback(char* topic, byte* payload, unsigned int length) {
   digitalWrite(LED_PIN, LOW);
   Serial.print(topic);
   Serial.print("-");
 
 
-  for(int i=0;i<length;i++){
+  for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
@@ -45,18 +51,18 @@ void callback(char* topic,byte* payload,unsigned int length){
   digitalWrite(LED_PIN, HIGH);
 }
 
-void reconnect(){
-  while(!client.connected()){
+void reconnect() {
+  while (!client.connected()) {
     Serial.print("正在连接MQTT服务器...");
 
 
-    String client_id = "ESP8266Client-"+ String(random(0xffff),HEX);
+    String client_id = "ESP8266Client-" + String(random(0xffff), HEX);
 
-    if(client.connect(client_id.c_str())){
+    if (client.connect(client_id.c_str())) {
       Serial.println("已连接");
-      digitalWrite(LED_PIN,HIGH);
+      digitalWrite(LED_PIN, HIGH);
       client.subscribe("control");  //订阅控制主题
-    }else{
+    } else {
       Serial.print("连接失败...,rc=");
       Serial.print(client.state());
       delay(5000);
@@ -64,21 +70,68 @@ void reconnect(){
   }
 }
 
+
+void handleSerialJson(const String& jsonStr) {
+  StaticJsonDocument<256> doc;  // 注意增大容量适应嵌套结构
+
+  DeserializationError error = deserializeJson(doc, jsonStr);
+  if (error) {
+    Serial.println("JSON解析失败");
+    return;
+  }
+
+  const char* topic = doc["topic"];
+  JsonObject msg = doc["msg"];
+
+  if (topic && !msg.isNull()) {
+    const char* option = msg["option"];
+    const char* value = msg["value"];
+
+    // Serial.print("topic: ");
+    // Serial.println(topic);
+    // Serial.print("option: ");
+    // Serial.println(option ? option : "NULL");
+    // Serial.print("value: ");
+    // Serial.println(value ? value : "NULL");
+
+    if (value) {
+      client.publish(topic, value);
+      Serial.println("已发布MQTT消息");
+    }
+  } else {
+    Serial.println("JSON字段不完整");
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED_PIN,OUTPUT);
-  digitalWrite(LED_PIN,HIGH);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
   setup_wifi();
 
-  client.setServer(mqtt_server,1883);
+  client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(!client.connected()){
+  if (!client.connected()) {
     reconnect();
   }
   client.loop();
+
+
+  //串口接收和缓存
+  while (Serial.available()) {
+    char c = Serial.read();
+
+    if (c == '#') {
+      handleSerialJson(serialBuffer);
+
+      serialBuffer = "";  //清空缓存
+    } else {
+      serialBuffer += c;
+    }
+  }
 }
